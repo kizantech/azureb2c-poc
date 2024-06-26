6dc6e4dd9d1e8b55b5f695edcc423e2eaaa6541a
Zack Way <zack.way@kizan.com>
2024-06-26 10:18:20 -0400

    Adding PowerBi Profile Id to Customer DB Context

--- src/AzureB2C.Data/Migrations/CustomerDb/20240626141756_AddPowerBiProfileId.Designer.cs
+++ src/AzureB2C.Data/Migrations/CustomerDb/20240626141756_AddPowerBiProfileId.Designer.cs
@@ -4,6 +4,7 @@ using AzureB2C.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AzureB2C.Data.Migrations.CustomerDb
 {
     [DbContext(typeof(CustomerDbContext))]
-    partial class CustomerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240626141756_AddPowerBiProfileId")]
+    partial class AddPowerBiProfileId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/AzureB2C.Data/Migrations/CustomerDb/20240626141756_AddPowerBiProfileId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AzureB2C.Data.Migrations.CustomerDb
+{
+    /// <inheritdoc />
+    public partial class AddPowerBiProfileId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
